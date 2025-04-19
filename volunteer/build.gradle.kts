9f8f928d57e4c16940a9c7662e7df078687334ac
egor4567888 <egor456788@gmail.com>
2025-04-19 18:24:11 +0300

    fix: :bug: fixed build.gradle and application.properties

--- volunteer/build.gradle.kts
+++ volunteer/build.gradle.kts
@@ -12,6 +12,11 @@ java {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
 
 repositories {
     mavenCentral()
@@ -21,6 +26,8 @@ dependencies {
     // --- Core Spring ---
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // --- DB ---
     runtimeOnly("com.h2database:h2") // H2 для разработки и тестов
@@ -32,6 +39,13 @@ dependencies {
     // --- Testing ---
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    
+
 }
 
 tasks.withType<Test> {
