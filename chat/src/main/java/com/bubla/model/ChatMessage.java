3fc7fb15919cd242b2cba9313be99aee5b92e7cc
ShablaBubla <rpervysin@gmail.com>
2025-04-19 19:25:39 +0300

    feat: :sparkles: added chat + models

---
+++ chat/src/main/java/com/bubla/model/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.bubla.model;
+
+
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    @Id
+    private String id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+    private String senderName;
+    private String getRecipientName;
+    private String content;
+    private Date timestamp;
+    private MessageStatus status;
+}
