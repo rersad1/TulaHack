35da34e668ff1fa3cdd27778a206f023ff47bc5f
ShablaBubla <rpervysin@gmail.com>
2025-04-20 00:16:04 +0300

    feat: :beers: added find by sender and recipient function

--- backend/chat/src/main/java/com/bubla/repository/ChatRoomRepository.java
+++ backend/chat/src/main/java/com/bubla/repository/ChatRoomRepository.java
@@ -3,5 +3,8 @@ package com.bubla.repository;
 import com.bubla.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
